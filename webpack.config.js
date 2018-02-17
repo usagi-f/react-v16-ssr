079b174db23d822e38dd262a03145859d632b447
usagi-f <usagi.f@gmail.com>
2018-02-17 22:10:53 +0900

    init: react app

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', { modules: false }],
+              'react'
+            ]
+          }
+        },
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/',
+    port: 9000
+  }
+};
